be32eff0778da8215cefda97ddca5da53f4395be
MOUNIMA SAI <saimounima@gmail.com>
2017-11-14 15:29:26 +0000

    Added config to randomize testcases

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,7 +36,10 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
+  
+   # Randomize the order test cases are executed.
+  config.active_support.test_order = :random
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
