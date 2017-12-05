70d8eb4cea80ef2498b4079e94655caa3d2b898e
Sai sankeerth Nomula <sankeerth@tamu.edu>
2017-12-05 03:38:57 +0000

    changed the local server to Heroku server for forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { :host => "forky.sankeerthsai279.c9users.io" }
+  config.action_mailer.default_url_options = { :host => "fork-friendly-app.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
