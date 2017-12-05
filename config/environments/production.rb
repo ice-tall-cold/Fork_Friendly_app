423a01f952291be9fa9439dfa7f2eb22e89c70aa
Sai sankeerth Nomula <sankeerth@tamu.edu>
2017-12-05 04:56:20 +0000

    Added smtp setting into prod.rb for heroku mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => "fork-friendly-app.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+
+  ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :user_name => 'forkfriendly1@gmail.com',
+  :password => 'forkfriendly!',
+  :enable_starttls_auto => true,
+  :domain => 'gmail.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
